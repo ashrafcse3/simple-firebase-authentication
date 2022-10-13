5864abb3a8e48348f43fabd3992ca9c1dd2a10be
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-13 20:58:24 +0100

    sign in with google

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import app from './firebase/firebase.init';
+import { useState } from 'react';
+
+const auth = getAuth(app);
 
 function App() {
+
+  const [user, setUser] = useState({});
+
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleSignInWithGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('sign out successful');
+        setUser({});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.uid ?
+          <button onClick={handleSignOut}>Sign out</button>
+          :
+          <button onClick={handleSignInWithGoogle}>Sign in with Google</button>
+      }
+
+      {
+        user.uid && <div>
+          <h3>User email: {user.email}</h3>
+          <p>Name: {user.displayName}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      }
+      {
+        user.uid || <div>
+          <h3>Login with any upper authentication system</h3>
+        </div>
+      }
     </div>
   );
 }
